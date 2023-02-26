5feed968672300093675065968bc5d6bc2d75fd0
kwang <k3lly.wang@gmail.com>
2023-02-26 13:58:35 -0500

    initial commit

---
+++ lesson-01/lesson-01.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+console.log(23+97);
+console.log(9+2+389+29+8+8.8);
+console.log((4 + 6 + 9) / 77);
+let max=57;
+let actual = max-13;
+let percentage = actual/max;
+console.log("percentage = ", percentage.toFixed(2));
